a24a5d718cc0b2c370a69c1e34c3a545ae306eae
lukasmyth96 <luka@nate.tech>
2021-03-13 12:58:42 +0000

    add CORS whitelist

--- webservice/main.py
+++ webservice/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from nuvox_algorithm import NuvoxAlgorithm
 from webservice.api import api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        'http://localhost:3000',
+        'http://192.168.1.193:3000',
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix='/api')
 
